2c7bac745b9c862dc1e89a3ecf6eb85e27093318
Arnab Chanda <arnabchanda964@gmail.com>
2019-10-23 02:13:40 +0530

    Added test ata and solution gen code

--- MyQuestions/Question2.py
+++ MyQuestions/Question2.py
@@ -3,7 +3,7 @@ from itertools import permutations
 from sys import stdin,stdout
 from bisect import bisect_left, bisect_right
 from copy import deepcopy
-from random import randint
+#from random import randint
 import sys
 
 int_input=lambda : int(stdin.readline())
@@ -14,61 +14,62 @@ list_input=lambda : list(map(int,stdin.readline().split()))
 string_list_input=lambda: list(string_input())
 MOD = pow(10,9)+7
 
-testfiles = 5
-for i in range(testfiles):
-    stdin = open('testfile_input'+str(i)+'.in','r')
-    sys.stdout = open('testfile_output'+str(i)+'.in','w')
+# testfiles = 5
+# for i in range(testfiles):
+#     stdin = open('testfile_input'+str(i)+'.in','r')
+#     sys.stdout = open('testfile_output'+str(i)+'.in','w')
 
-    test  = int_input()
+#solution
+test  = int_input()
 
-    for _ in range(test): 
-        tony = []
-        thanos = []
-        n,k = multi_int_input()
-        #print(n,k)
-        tony = list_input()
-        thanos = list_input()
+for _ in range(test): 
+    tony = []
+    thanos = []
+    n,k = multi_int_input()
+    #print(n,k)
+    tony = list_input()
+    thanos = list_input()
 
-        list_numbers = []
-        count_list = []
-        li = deque(list_numbers)
-        count_list = deque(count_list)
+    list_numbers = []
+    count_list = []
+    li = deque(list_numbers)
+    count_list = deque(count_list)
 
-        present_elements = {}
+    present_elements = {}
+    
+    for i in range(0,n):
+        #Tony's turn 
+        if tony[i] not in present_elements or present_elements[tony[i]] == 0:
+            if len(li) == k:
+                x = li.popleft()
+                present_elements[x] = 0
+                count_list.popleft()  
+            li.append(tony[i])
+            count_list.append(1)
+            present_elements[tony[i]] = 1
         
-        for i in range(0,n):
-            #Tony's turn 
-            if tony[i] not in present_elements or present_elements[tony[i]] == 0:
-                if len(li) == k:
-                    x = li.popleft()
-                    present_elements[x] = 0
-                    count_list.popleft()  
-                li.append(tony[i])
-                count_list.append(1)
-                present_elements[tony[i]] = 1
-            
-            #Thanos's turn
-            if thanos[i] not in present_elements or present_elements[thanos[i]] == 0:
-                if len(li) == k:
-                    x = li.pop()
-                    present_elements[x] = 0
-                    count_list.pop()  
-                li.appendleft(thanos[i])
-                count_list.appendleft(0)
-                present_elements[thanos[i]] = 1
-        
-        no_tony = 0 
-        no_thanos = 0
+        #Thanos's turn
+        if thanos[i] not in present_elements or present_elements[thanos[i]] == 0:
+            if len(li) == k:
+                x = li.pop()
+                present_elements[x] = 0
+                count_list.pop()  
+            li.appendleft(thanos[i])
+            count_list.appendleft(0)
+            present_elements[thanos[i]] = 1
+    
+    no_tony = 0 
+    no_thanos = 0
 
-        for i in count_list:
-            if i == 1:
-                no_tony+=1
-            elif i == 0:
-                no_thanos+=1
-        
-        if no_tony>no_thanos:
-            print('TONY')
-        elif no_tony<no_thanos:
-            print('THANOS')
-        else:
-            print('RUN TONY')
+    for i in count_list:
+        if i == 1:
+            no_tony+=1
+        elif i == 0:
+            no_thanos+=1
+    
+    if no_tony>no_thanos:
+        print('TONY')
+    elif no_tony<no_thanos:
+        print('THANOS')
+    else:
+        print('RUN TONY')
